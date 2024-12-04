1eaa478a25044f0b25233201b8c8a72312579437
YisakH <drm05003@naver.com>
2024-12-04 16:39:38 +0900

    Trigger github actions

--- ticket-db/src/main/java/com/ticket/ticketdb/TicketDbApplication.java
+++ ticket-db/src/main/java/com/ticket/ticketdb/TicketDbApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TicketDbApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TicketDbApplication.class, args);
     }
-
 }
